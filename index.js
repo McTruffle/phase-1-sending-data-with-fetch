37aa832c02247c1a55aa3eab7b97cb8a6627a012
McTruffle <56895893+McTruffle@users.noreply.github.com>
2022-06-28 23:40:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+
+// Add your code here
+function submitData(name, email){
+    let postRequest = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept" : "application/json",
+        },
+        body: JSON.stringify(name, email)
+    };
+    fetch('http://localhost:3000/users', postRequest)
+    .then (res => res.json())
+    .then (info => console.log(info))
+    .catch (e => {
+        alert("Something went wrong!")
+        console.log(e.message)
+    })
+};
